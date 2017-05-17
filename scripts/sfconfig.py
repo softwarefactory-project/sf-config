4d7247fdf07931e6131e53991aadceaea6494868
Tristan Cacqueray <tdecacqu@redhat.com>
2017-05-17 11:46:56 +0000

    zuul: add database reporter
    Depends-On: I5a12cb857e8d8ffe1e88a2d841f4f73b41b4a117
    Change-Id: If26a1753d35e4fb74002d5382b23f7fdf0d7694c
    Related-Task: 386

--- scripts/sfconfig.py
+++ scripts/sfconfig.py
@@ -551,6 +551,12 @@ DNS.1 = %s
             get_hostname("zuul"), defaults["zuul_port"])
         # TODO(tristanC): create a dedicated key for zuul
         glue["zuul_rsa_pub"] = glue["jenkins_rsa_pub"]
+        glue["zuul_mysql_host"] = glue["mysql_host"]
+        glue["mysql_databases"]["zuul"] = {
+            'hosts': ["localhost", get_hostname("zuul")],
+            'user': 'zuul',
+            'password': secrets['zuul_mysql_password'],
+        }
 
     if "nodepool" in arch["roles"]:
         glue["nodepool_providers"] = sfconfig["nodepool"].get("providers", [])
