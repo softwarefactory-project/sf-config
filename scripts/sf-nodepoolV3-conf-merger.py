d9c52b082e8ed6a94e06ff398f68ef9b7ec42fd2
Tristan Cacqueray <tdecacqu@redhat.com>
2017-05-20 04:40:32 +0000

    ZuulV3 and NodepoolV3
    This change adds a new zuul3 and nodepool3 to deploy the new services.
    
    Depends-On: I9550fe6d15efecda0956f0fc10122e6e060b154e
    Depends-On: Ia2a87df95e49ca624e9312d99b9ece147260c71d
    Change-Id: I433ea127eb7e6d2309f092ccde3c363b6942050e

---
+++ scripts/sf-nodepoolV3-conf-merger.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import yaml
+
+
+def merge(_nodepool):
+    conf = yaml.safe_load(open(_nodepool))
+    user = yaml.safe_load(open("nodepool/nodepoolV3.yaml"))
+
+    for provider in user['providers']:
+        if not provider.get("image-type"):
+            provider["image-type"] = "raw"
+
+    for dib in user['diskimages']:
+        envvars = dib.setdefault('env-vars', {})
+        envvars['TMPDIR'] = '/var/cache/nodepool/dib_tmp'
+        envvars['DIB_IMAGE_CACHE'] = '/var/cache/nodepool/dib_cache'
+        envvars['DIB_GRUB_TIMEOUT'] = '0'
+        envvars['DIB_CHECKSUM'] = '1'
+        # Make sure env-vars are str
+        for k, v in envvars.items():
+            if not isinstance(v, basestring):
+                envvars[k] = str(v)
+
+    if 'cron' in user:
+        conf['cron'] = user['cron']
+    conf['labels'] = user['labels']
+    conf['providers'] = user['providers']
+    conf['diskimages'] = user['diskimages']
+    return yaml.dump(conf, default_flow_style=False)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "Please provide the target filename"
+        sys.exit(1)
+    out = sys.argv[1]
+    _nodepool = "%s/_nodepool.yaml" % os.path.dirname(out)
+    for reqfile in (_nodepool, "nodepool/nodepool.yaml"):
+        if not os.path.isfile(reqfile):
+            print "%s: missing file" % reqfile
+            sys.exit(1)
+    merged = merge(_nodepool)
+    open(out, 'w').write(merged)
