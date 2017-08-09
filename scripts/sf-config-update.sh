3f25d9cb5eb9fcbd2075bda8e369c1822cac3025
Fabien Boucher <fboucher@redhat.com>
2017-08-09 12:46:00 +0000

    Display sf_configrepo_update.yml output in the config-update console too
    Change-Id: I9a970a032320f3205841cc21a00f883f74c10460

--- scripts/sf-config-update.sh
+++ scripts/sf-config-update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-ACTION=${SSH_ORIGINAL_COMMAND:-sf_configrepo_update}
+ACTION="$SSH_ORIGINAL_COMMAND"
 
 export ANSIBLE_CONFIG=/usr/share/sf-config/ansible/ansible.cfg
 
 case $ACTION in
     sf_configrepo_update)
-        exec ansible-playbook /var/lib/software-factory/ansible/sf_configrepo_update.yml &> /var/log/software-factory/configrepo_update.log
+        exec ansible-playbook /var/lib/software-factory/ansible/sf_configrepo_update.yml | tee -a /var/log/software-factory/configrepo_update.log
         ;;
     sf_mirror_update)
         exec ansible-playbook -v /usr/share/sf-config/ansible/roles/sf-mirror/files/update_playbook.yml &> /var/log/software-factory/mirror_update.log
