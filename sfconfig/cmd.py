e39b243cf497eb704c75360587e50a7f0eea3d35
Tristan Cacqueray <tdecacqu@redhat.com>
2017-08-11 07:40:32 +0000

    sfconfig: refactor into a python module
    Depends-On: I74e9795a0e684c42bfa3cc7d2d25657c6eda9749
    Change-Id: I12be1751433c4edf67832e8654dddf02eb87d885

--- sfconfig/cmd.py
+++ sfconfig/cmd.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
-# Licensed under the Apache License, Version 2.0
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 #
 # Generate ansible group vars based on refarch and sfconfig.yaml
 
@@ -8,7 +18,6 @@ import base64
 import os
 import random
 import subprocess
-import string
 import sys
 import shutil
 import time
@@ -17,6 +26,9 @@ import yaml
 
 from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
+
+from sfconfig.upgrade import update_sfconfig
+from sfconfig.upgrade import update_arch
 
 
 required_roles = (
@@ -156,289 +168,6 @@ def load_refarch(filename, domain=None, install_server_ip=None):
     arch["install"] = install_host["hostname"]
     arch["install_ip"] = install_host["ip"]
     return arch
-
-
-def update_sfconfig(data, args):
-    """ This method ensure /etc/software-factory content is upgraded """
-    dirty = False
-    if not os.path.isfile("/etc/software-factory/logo-topmenu.png"):
-        open("/etc/software-factory/logo-topmenu.png", "w").write(
-            base64.decodestring(data['theme']['topmenu_logo_data']))
-        del data['theme']['topmenu_logo_data']
-        dirty = True
-
-    if not os.path.isfile("/etc/software-factory/logo-favicon.ico"):
-        open("/etc/software-factory/logo-favicon.ico", "w").write(
-            base64.decodestring(data['theme']['favicon_data']))
-        del data['theme']['favicon_data']
-        dirty = True
-
-    if not os.path.isfile("/etc/software-factory/logo-splash.png"):
-        open("/etc/software-factory/logo-splash.png", "w").write(
-            base64.decodestring(data['theme']['splash_image_data']))
-        del data['theme']['splash_image_data']
-
-    # 2.2.3: remove service list (useless since arch.yaml)
-    if 'services' in data:
-        del data['services']
-        dirty = True
-
-    # Make sure mirrors is in the conf
-    if 'mirrors' not in data:
-        data['mirrors'] = {
-            'swift_mirror_url': 'http://swift:8080/v1/AUTH_uuid/repomirror/',
-            'swift_mirror_tempurl_key': 'CHANGEME',
-        }
-        dirty = True
-
-    # Remove it after 2.6.0 upgrade
-    if 'welcome_path_path' not in data:
-        data['welcome_page_path'] = 'sf/welcome.html'
-        dirty = True
-
-    # 2.2.4: refactor OAuth2 and OpenID auth config
-    if 'oauth2' not in data['authentication']:
-        data['authentication']['oauth2'] = {
-            'github': {
-                'disabled': False,
-                'client_id': '',
-                'client_secret': '',
-                'github_allowed_organizations': ''
-            },
-            'google': {
-                'disabled': False,
-                'client_id': '',
-                'client_secret': ''
-            },
-            'bitbucket': {
-                'disabled': True,
-                'client_id': '',
-                'client_secret': ''
-            },
-        }
-        dirty = True
-
-    if 'openid' not in data['authentication']:
-        data['authentication']['openid'] = {
-            'disabled': False,
-            'server': 'https://login.launchpad.net/+openid',
-            'login_button_text': 'Log in with the Launchpad service'
-        }
-        dirty = True
-
-    if data['authentication'].get('github'):
-        (data['authentication']['oauth2']
-         ['github']['disabled']) = (data['authentication']['github']
-                                    ['disabled'])
-        (data['authentication']['oauth2']
-         ['github']['client_id']) = (data['authentication']['github']
-                                     ['github_app_id'])
-        (data['authentication']['oauth2']
-         ['github']['client_secret']) = (data['authentication']['github']
-                                         ['github_app_secret'])
-        (data['authentication']['oauth2']['github']
-         ['github_allowed_organizations']) = (data['authentication']
-                                              ['github']
-                                              ['github_allowed_organizations'])
-        if data['authentication']['github'].get('redirect_uri'):
-            (data['authentication']['oauth2']['github']
-             ['redirect_uri']) = string.replace(data['authentication']
-                                                ['github']['redirect_uri'],
-                                                "login/github/callback",
-                                                "login/oauth2/callback")
-        del data['authentication']['github']
-        dirty = True
-
-    if data['authentication'].get('launchpad'):
-        (data['authentication']['openid']
-         ['disabled']) = data['authentication']['launchpad']['disabled']
-        if data['authentication']['launchpad'].get('redirect_uri'):
-            (data['authentication']['openid']
-             ['redirect_uri']) = (data['authentication']['launchpad']
-                                  ['redirect_uri'])
-        del data['authentication']['launchpad']
-        dirty = True
-
-    if 'periodic_update' not in data['mirrors']:
-        data['mirrors']['periodic_update'] = False
-        dirty = True
-    if 'swift_mirror_ttl' not in data['mirrors']:
-        data['mirrors']['swift_mirror_ttl'] = 15811200
-        dirty = True
-
-    if 'use_letsencrypt' not in data['network']:
-        data['network']['use_letsencrypt'] = False
-
-    # Mumble is enable when the role is defined in arch
-    if 'disabled' in data['mumble']:
-        del data['mumble']['disabled']
-        dirty = True
-
-    # 2.2.5: finished arch aware top-menu, remove service toggle now
-    for hideable in ('redmine', 'etherpad', 'paste'):
-        key = 'topmenu_hide_%s' % hideable
-        if key in data['theme']:
-            del data['theme'][key]
-            dirty = True
-
-    # 2.2.6: enforce_ssl is enabled by default
-    if 'enforce_ssl' in data['network']:
-        del data['network']['enforce_ssl']
-        dirty = True
-
-    # 2.2.7: add openid_connect settings
-    if 'openid_connect' not in data['authentication']:
-        data['authentication']['openid_connect'] = {
-            'disabled': True,
-            'issuer_url': None,
-            'client_secret': None,
-            'client_id': None,
-            'login_button_text': 'Log in with OpenID Connect'
-        }
-        dirty = True
-
-    # 2.2.7 rename variables for jinja2 templates
-    old_names = ["auth-url", "project-id", "max-servers", "boot-timeout"]
-    for value in data['nodepool']['providers']:
-        for name in old_names:
-            if name in value:
-                value[name.replace('-', '_')] = value.pop(name)
-                dirty = True
-
-    # 2.2.7: add oidc field mapping, default to google values
-    if 'mapping' not in data['authentication']['openid_connect']:
-        data['authentication']['openid_connect']['mapping'] = {
-            'login': 'email',
-            'email': 'email',
-            'name': 'name',
-            'uid': 'sub',
-            'ssh_keys': None
-        }
-
-    # 2.3.0: enable static hosts settings
-    if 'static_hostnames' not in data['network']:
-        data['network']['static_hostnames'] = []
-        dirty = True
-
-    # 2.3.0: add debug setting
-    if 'debug' not in data:
-        data['debug'] = False
-        dirty = True
-
-    if "disabled" in data['nodepool']:
-        # Disable provider if disable
-        if data["nodepool"]["disabled"] and data['nodepool'].get('providers'):
-            data['nodepool']['providers'][0]['auth_url'] = ""
-        del data['nodepool']['disabled']
-        dirty = True
-
-    for provider in data['nodepool'].get('providers', []):
-        for key in ('boot_timeout', 'max_servers', 'network', 'pool', 'rate'):
-            if key in provider:
-                del provider[key]
-                dirty = True
-        if 'project_id' in provider:
-            provider['project_name'] = provider['project_id']
-            del provider['project_id']
-            dirty = True
-
-    # 2.6.0: add scp backup parameters
-    if 'method' not in data['backup']:
-        data['backup']['method'] = 'swift'
-        data['backup']['scp_backup_host'] = 'remoteserver.sftests.com'
-        data['backup']['scp_backup_port'] = 22
-        data['backup']['scp_backup_user'] = 'root'
-        data['backup']['scp_backup_directory'] = '/var/lib/remote_backup'
-        data['backup']['scp_backup_max_retention_secs'] = 864000
-        dirty = True
-
-    if 'zuul' not in data:
-        data['zuul'] = {
-            'external_logservers': [],
-            'default_log_site': "sflogs",
-            'log_url': "",
-        }
-        dirty = True
-
-    if 'static_nodes' not in data:
-        data['zuul']['static_nodes'] = []
-        dirty = True
-
-    if 'gerrit_connections' not in data['zuul']:
-        data['zuul']['gerrit_connections'] = []
-        dirty = True
-
-    if "gerrit_connections" in data:
-        for gerrit_connection in data["gerrit_connections"]:
-            if not gerrit_connection["disabled"]:
-                if 'disabled' in gerrit_connection:
-                    del gerrit_connection['disabled']
-                data['zuul']['gerrit_connections'].append(gerrit_connection)
-        del data["gerrit_connections"]
-
-    # 2.6.0: expose elasticsearch config
-    if 'elasticsearch' not in data:
-        data['elasticsearch'] = {}
-        dirty = True
-    if 'heap_size' not in data['elasticsearch']:
-        data['elasticsearch']['heap_size'] = '512m'
-        dirty = True
-    if 'replicas' not in data['elasticsearch']:
-        data['elasticsearch']['replicas'] = 0
-        dirty = True
-
-    # 2.6.0: expose logstash config
-    if 'logstash' not in data:
-        data['logstash'] = {}
-        dirty = True
-    if 'retention_days' not in data['logstash']:
-        data['logstash']['retention_days'] = 60
-
-    if 'disable_external_resources' not in data['network']:
-        data['network']['disable_external_resources'] = False
-        dirty = True
-
-    if data['network']['disable_external_resources'] != \
-       args.disable_external_resources:
-        data['network']['disable_external_resources'] = \
-            args.disable_external_resources
-        dirty = True
-
-    return dirty
-
-
-def clean_arch(data):
-    dirty = False
-    # Rename auth in cauth
-    for host in data['inventory']:
-        if 'auth' in host['roles']:
-            host['roles'].remove('auth')
-            host['roles'].append('cauth')
-            dirty = True
-        # Remove legacy roles
-        if 'zuul' in host['roles']:
-            host['roles'].remove('zuul')
-            host['roles'].append('zuul-server')
-            dirty = True
-        if 'nodepool' in host['roles']:
-            host['roles'].remove('nodepool')
-            host['roles'].append('nodepool-launcher')
-            dirty = True
-    # Remove data added *IN-PLACE* by utils_refarch
-    # Those are now saved in _arch.yaml instead
-    for dynamic_key in ("domain", "gateway", "gateway_ip", "install",
-                        "install_ip", "ip_prefix", "roles", "hosts_file"):
-        if dynamic_key in data:
-            del data[dynamic_key]
-            dirty = True
-
-    # Remove deployments related information
-    for deploy_key in ("cpu", "mem", "hostid", "rolesname", "hostname"):
-        for host in data["inventory"]:
-            if deploy_key in host:
-                del host[deploy_key]
-                dirty = True
-    return dirty
 
 
 def get_sf_version():
@@ -1167,7 +896,7 @@ def main():
     sfarch = yaml_load(args.arch)
     if update_sfconfig(sfconfig, args):
         save_file(sfconfig, args.sfconfig)
-    if clean_arch(sfarch):
+    if update_arch(sfarch):
         save_file(sfarch, args.arch)
 
     if not args.use_new_arch and args.recover and len(sfarch["inventory"]) > 1:
