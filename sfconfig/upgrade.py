5923569be083197018b98fdd3650c227c5919cd1
Tristan Cacqueray <tdecacqu@redhat.com>
2017-08-11 07:40:32 +0000

    sfconfig: refactor into a python module
    This change distributes the sfconfig code in simple python module.
    
    Depends-On: I74e9795a0e684c42bfa3cc7d2d25657c6eda9749
    Change-Id: I12be1751433c4edf67832e8654dddf02eb87d885

---
+++ sfconfig/upgrade.py
@@ -0,0 +1,102 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def update_sfconfig(data, args):
+    """ This method ensure /etc/software-factory content is upgraded """
+    dirty = False
+
+    # 2.6.0: add scp backup parameters
+    if 'method' not in data['backup']:
+        data['backup']['method'] = 'swift'
+        data['backup']['scp_backup_host'] = 'remoteserver.sftests.com'
+        data['backup']['scp_backup_port'] = 22
+        data['backup']['scp_backup_user'] = 'root'
+        data['backup']['scp_backup_directory'] = '/var/lib/remote_backup'
+        data['backup']['scp_backup_max_retention_secs'] = 864000
+        dirty = True
+
+    if 'zuul' not in data:
+        data['zuul'] = {
+            'external_logservers': [],
+            'default_log_site': "sflogs",
+            'log_url': "",
+        }
+        dirty = True
+
+    if 'static_nodes' not in data:
+        data['zuul']['static_nodes'] = []
+        dirty = True
+
+    if 'gerrit_connections' not in data['zuul']:
+        data['zuul']['gerrit_connections'] = []
+        dirty = True
+
+    if "gerrit_connections" in data:
+        for gerrit_connection in data["gerrit_connections"]:
+            if not gerrit_connection["disabled"]:
+                if 'disabled' in gerrit_connection:
+                    del gerrit_connection['disabled']
+                data['zuul']['gerrit_connections'].append(gerrit_connection)
+        del data["gerrit_connections"]
+
+    # 2.6.0: expose elasticsearch config
+    if 'elasticsearch' not in data:
+        data['elasticsearch'] = {}
+        dirty = True
+    if 'heap_size' not in data['elasticsearch']:
+        data['elasticsearch']['heap_size'] = '512m'
+        dirty = True
+    if 'replicas' not in data['elasticsearch']:
+        data['elasticsearch']['replicas'] = 0
+        dirty = True
+
+    # 2.6.0: expose logstash config
+    if 'logstash' not in data:
+        data['logstash'] = {}
+        dirty = True
+    if 'retention_days' not in data['logstash']:
+        data['logstash']['retention_days'] = 60
+
+    if 'disable_external_resources' not in data['network']:
+        data['network']['disable_external_resources'] = False
+        dirty = True
+
+    if data['network']['disable_external_resources'] != \
+       args.disable_external_resources:
+        data['network']['disable_external_resources'] = \
+            args.disable_external_resources
+        dirty = True
+
+    return dirty
+
+
+def update_arch(data):
+    dirty = False
+    for host in data['inventory']:
+        # Remove legacy roles
+        if 'zuul' in host['roles']:
+            host['roles'].remove('zuul')
+            host['roles'].append('zuul-server')
+            dirty = True
+        if 'nodepool' in host['roles']:
+            host['roles'].remove('nodepool')
+            host['roles'].append('nodepool-launcher')
+            dirty = True
+
+    # Remove deployments related information
+    for deploy_key in ("cpu", "mem", "hostid", "rolesname", "hostname"):
+        for host in data["inventory"]:
+            if deploy_key in host:
+                del host[deploy_key]
+                dirty = True
+    return dirty
