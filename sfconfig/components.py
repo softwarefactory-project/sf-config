28fa6fd39959b3eda8bf07b7269d19d821d2fd92
Tristan Cacqueray <tdecacqu@redhat.com>
2017-08-14 02:20:08 +0000

    sfconfig: refactor into components
    * Replace {{ domain }} by {{ fqdn }}
    * Refactor arch variable
    * Store all variables in the args dictionary
    * Refactor role setings to Component class
    
    Story: 219
    Task: 547
    Change-Id: I90dd05add900ab94a9154e5edfc90db651769cab

---
+++ sfconfig/components.py
@@ -0,0 +1,69 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+
+from sfconfig.utils import execute
+
+
+class Component(object):
+    require_roles = []
+
+    def get_or_generate_ssh_key(self, args, name):
+        priv = "%s/ssh_keys/%s" % (args.lib, name)
+        pub = "%s/ssh_keys/%s.pub" % (args.lib, name)
+
+        if not os.path.isfile(priv):
+            execute(["ssh-keygen", "-t", "rsa", "-N", "", "-f", priv, "-q"])
+        args.glue[name] = open(priv).read()
+        args.glue["%s_pub" % name] = open(pub).read()
+
+    def add_mysql_database(self, args, name, hosts=[],
+                           user=None, password=None):
+        # Always enable localhost access
+        if "localhost" not in hosts:
+            hosts.append("localhost")
+        service_host = args.glue["%s_host" % name]
+        if service_host not in hosts:
+            hosts.append(service_host)
+        if user is None:
+            user = name
+        if password is None:
+            password = args.secrets["%s_mysql_password" % name]
+        args.glue.setdefault("mysql_databases", {})[name] = {
+            'hosts': hosts,
+            'user': user,
+            'password': password,
+        }
+        args.glue["%s_mysql_host" % name] = args.glue["mysql_host"]
+
+    def argparse(self, parser):
+        pass
+
+    def prepare(self, args):
+        missing_roles = set()
+        for role in self.require_roles:
+            if [True for host in args.sfarch["inventory"]
+                    if role not in host["roles"]]:
+                missing_roles.add(role)
+        if not args.allinone and missing_roles:
+            raise RuntimeError("%s: missing required role %s" % (
+                self.role, missing_roles))
+        elif missing_roles:
+            print("%s: Adding missing roles %s" % (self.role,
+                                                   " ".join(missing_roles)))
+            for role in missing_roles:
+                args.sfarch["inventory"][0]["roles"].append(role)
+            args.save_arch = True
+
+    def configure(self, args, host):
+        pass
