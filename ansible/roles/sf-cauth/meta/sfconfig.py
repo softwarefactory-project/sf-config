98eaab0c3b01dd80bbca6936ceaed629d8ddc63b
Tristan Cacqueray <tdecacqu@redhat.com>
2017-08-14 02:20:08 +0000

    sfconfig: refactor into components
    * Replace {{ domain }} by {{ fqdn }}
    * Refactor arch variable
    * Store all variables in the args dictionary
    * Refactor role setings to Component class
    
    Story: 219
    Task: 547
    Change-Id: I90dd05add900ab94a9154e5edfc90db651769cab

---
+++ ansible/roles/sf-cauth/meta/sfconfig.py
@@ -0,0 +1,30 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+
+from sfconfig.components import Component
+from sfconfig.utils import execute
+
+
+class Cauth(Component):
+    def configure(self, args, host):
+        priv_file = "%s/certs/cauth_privkey.pem" % args.lib
+        pub_file = "%s/certs/cauth_pubkey.pem" % args.lib
+        if not os.path.isfile(priv_file):
+            execute(["openssl", "genrsa", "-out", priv_file, "1024"])
+        if not os.path.isfile(pub_file):
+            execute(["openssl", "rsa", "-in", priv_file, "-out", pub_file,
+                     "-pubout"])
+        args.glue["cauth_privkey"] = open(priv_file).read()
+        args.glue["cauth_pubkey"] = open(pub_file).read()
+        self.add_mysql_database(args, "cauth")
---
+++ ansible/roles/sf-logserver/meta/sfconfig.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from sfconfig.components import Component
+
+
+class LogServer(Component):
+    def prepare(self, args):
+        super(LogServer, self).prepare(args)
+        args.glue["loguser_authorized_keys"] = []
+
+    def configure(self, args, host):
+        args.glue["logservers"].append({
+            "name": "sflogs",
+            "host": args.glue["logserver_host"],
+            "user": "loguser",
+            "path": "/var/www/logs",
+        })
+        if args.glue["logserver_host"] != args.glue["install_server_host"]:
+            args.glue["zuul_ssh_known_hosts"].append({
+                "host_packed": args.glue["logserver_host"],
+                "host": args.glue["logserver_host"],
+                "port": 22
+            })
