42ef89a736e4fc18d0c6c49f0221a301b1a89727
Tristan Cacqueray <tdecacqu@redhat.com>
2017-09-07 01:56:01 +0000

    recover: use local arch by default
    This change remove the --use-new-arch argument and do it by default.
    Using the arch from the backup doesn't make sense because when doing
    a recover, the new instance should already be deployed with the arch
    it needs.
    
    Change-Id: Ifd78e769214182433b857eee81e2ea8af21d21de

--- sfconfig/cmd.py
+++ sfconfig/cmd.py
@@ -52,14 +52,12 @@ def copy_backup(backup):
 
 
 def bootstrap_backup(use_new_arch=False):
-    # Install sfconfig and arch in place
-    shutil.copy("%s/install-server/etc/software-factory/sfconfig.yaml" % bdir,
-                "/etc/software-factory/sfconfig.yaml")
-    if not use_new_arch:
-        shutil.copy("%s/install-server/etc/software-factory/arch-backup.yaml" %
-                    bdir, "/etc/software-factory/arch.yaml")
+    # Copy software-factory conf
+    execute(["rsync", "-a", "--exclude", "arch.yaml",
+             "%s/install-server/etc/software-factory/" % bdir,
+             "/etc/software-factory/"])
     # Copy bootstrap data
-    execute(["rsync", "-a",
+    execute(["rsync", "-a", "--exclude", "arch.yaml",
              "%s/install-server/var/lib/software-factory/" % bdir,
              "/var/lib/software-factory/"])
     print("Boostrap data prepared from the backup. Done.")
@@ -102,8 +100,6 @@ def usage(components):
     # special actions
     p.add_argument("--recover", nargs='?', const=bdir, metavar='BACKUP_PATH',
                    help="Deploy a backup")
-    p.add_argument("--use-new-arch", action='store_true',
-                   help="When restoring a backup, keep the arch setup by heat")
     p.add_argument("--disable", action='store_true', help="Turn off services")
     p.add_argument("--erase", action='store_true', help="Erase data")
     p.add_argument("--upgrade", action='store_true', help="Run upgrade task")
@@ -164,11 +160,6 @@ def main():
     # Make sure the yaml files are updated
     sfconfig.upgrade.update_sfconfig(args)
     sfconfig.upgrade.update_arch(args)
-
-    if not args.use_new_arch and args.recover and \
-       len(args.sfarch["inventory"]) > 1:
-        print("Make sure ip addresses in %s are correct" % args.arch)
-        raw_input("Press enter to continue")
 
     # Parse components options
     for component in components.values():
