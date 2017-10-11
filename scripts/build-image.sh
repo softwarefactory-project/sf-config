3637223f701ed4d9aab1f96d0b02a4d80738493c
Nicolas Hicher <nhicher@redhat.com>
2017-10-11 15:56:27 -0400

    Add script to build image using virt-customize
    Change-Id: I1b985990651dc6054ed4ceb0370174b171689912

---
+++ scripts/build-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+curl -o sf-master.qcow2 https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
+sudo yum install -y libguestfs-tools
+virt-customize -a sf-master.qcow2 --selinux-relabel \
+               --run-command "yum install -y https://softwarefactory-project.io/repos/sf-release-master.rpm" \
+               --run-command "yum update -y" \
+               --run-command "yum install --downloadonly sf-config --downloaddir=/tmp/" \
+               --run-command "yum install -y /tmp/sf-config*" \
+               --run-command "sed -i '/populate_hosts/d' /usr/lib/python2.7/site-packages/sfconfig/inventory.py" \
+               --run-command "sed -i '/    config_update(args, pb)/d' /usr/lib/python2.7/site-packages/sfconfig/inventory.py" \
+               --run-command "sed -i '/    postconf(args, pb)/d' /usr/lib/python2.7/site-packages/sfconfig/inventory.py" \
+               --run-command "mkdir /dev/shm && mount -t tmpfs shmfs /dev/shm" \
+               --run-command "sfconfig --skip-setup --skip-test --arch /usr/share/sf-config/refarch/allinone.yaml" \
+               --run-command "rm -rf /var/lib/software-factory/*" \
+               --run-command "yum reinstall -y /tmp/sf-config*"
