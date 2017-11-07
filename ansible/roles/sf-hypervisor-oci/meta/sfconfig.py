530da731165d28635f443f909aab638a991f5700
Tristan Cacqueray <tdecacqu@redhat.com>
2017-11-07 07:01:59 +0000

    hypervisor-oci: prevent running on the control plane
    This change adds the --enabel-insecure-slave sfconfig argument to authorize
    using the hypervisor-oci role on the control plane.
    
    TG-659
    
    Depends-On: I7c9ed17f41b40074910fc1aebbd657afeca14c07
    Change-Id: I4c660e69005a640eb9c7d8671ef7278b01bff3bf

---
+++ ansible/roles/sf-hypervisor-oci/meta/sfconfig.py
@@ -0,0 +1,36 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from sfconfig.components import Component
+
+
+class HypervisorOci(Component):
+    role = "hypervisor-oci"
+
+    def usage(self, parser):
+        parser.add_argument("--enable-insecure-slave", action='store_true',
+                            help="Allow using hypervisor-oci on the "
+                            "control plane")
+
+    def argparse(self, args):
+        if args.enable_insecure_slave:
+            args.glue["enable_insecure_slave"] = True
+
+    def validate(self, args, host):
+        if host["roles"] != ["hypervisor-oci"] and \
+           args.glue.get("enable_insecure_slave") is not True:
+            print("Can not deploy hypervisor-oci on %s" % host["hostname"])
+            print("This host is part of the control-plane, use "
+                  "--enable-insecure-slave argument to continue. ")
+            print("See https://softwarefactory-project.io/docs/operator/"
+                  "nodepool_operator.html#container-provider")
+            exit(1)
