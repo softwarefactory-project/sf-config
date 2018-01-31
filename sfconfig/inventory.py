8097ef85542c1e20437203ae8550ae0abb68fd0a
Nicolas Hicher <nhicher@redhat.com>
2018-01-31 13:25:13 -0500

    metrics: Add testinfra for all components
    add testinfra file for influxdb, grafana, graphite-api and telegraf
    
    Change-Id: I12cc04fedd204f2f0e55dc9443cd20c0c31939f6

--- sfconfig/inventory.py
+++ sfconfig/inventory.py
@@ -304,6 +304,8 @@ def enable_action(args):
             for role in host["roles"]:
                 if role in testinfra_tests:
                     tests.append(role)
+            if 'influxdb' or 'graphite-api' in role:
+                tests.append('telegraf')
             if tests:
                 for test in tests:
                     testinfra.append(testinfra_tests[test])
