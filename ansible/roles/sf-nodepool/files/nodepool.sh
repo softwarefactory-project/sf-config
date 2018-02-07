809557217fa90a7265fc96b60ec0f8759f19ea33
Tristan Cacqueray <tdecacqu@redhat.com>
2018-02-07 08:49:17 +0000

    nodepool: use new statsd configuration option
    This change also finish to fix zuul3/nodepool3 scl path name and removes
    the now deprecated sysconfig files.
    
    Change-Id: I734e8489a1ca30a4b02d6f349dbdec84d32c6723

--- ansible/roles/sf-nodepool/files/nodepool.sh
+++ ansible/roles/sf-nodepool/files/nodepool.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-exec scl enable rh-python35 -- /opt/rh/rh-python35/root/bin/nodepool -c /etc/opt/rh/rh-python35/nodepool/nodepool.yaml $*
+exec scl enable rh-python35 -- /opt/rh/rh-python35/root/bin/nodepool \
+     -s /etc/opt/rh/rh-python35/nodepool/secure.conf \
+     -c /etc/opt/rh/rh-python35/nodepool/nodepool.yaml $*
