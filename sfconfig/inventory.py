f15799cc6cdf20de73d432f40d803652ca414f89
Fabien Boucher <fboucher@redhat.com>
2018-02-26 18:41:17 +0000

    sf-config integrate DLRN
    DLRN projects can be defined in config/dlrn/projects.yaml.
    Multiple DLRN projects can be configured and sfconfig will
    make sure to set configuration file and path segemented by
    DLRN projects. Thus it shoulds possible to run multiple
    DLRN instance on the same DLRN node.
    
    By DLRN project via the gateway:
    * https://sftests.com/dlrn/<project>: DLRN data/repos
    * https://sftests.com/dlrn-api/<project> DLRN API
    
    Depends-on: I357c8f7928342b27cbf1fc78193f94789233369f
    
    Change-Id: I3cab90b56739977396e0942ae932aee7f731cd98

--- sfconfig/inventory.py
+++ sfconfig/inventory.py
@@ -223,7 +223,7 @@ def config_update(args, pb):
     # The list of role to run update task
     roles_order = ["gerrit", "pages", "gerritbot",
                    "gateway", "managesf", "mirror", "repoxplorer",
-                   "zuul", "nodepool", "grafana", "hound"]
+                   "zuul", "nodepool", "grafana", "hound", "dlrn"]
     # The extra list of host group to run fetch-config-repo
     roles_group = [
         "zuul-scheduler", "zuul-merger", "zuul-executor", "zuul-web",
