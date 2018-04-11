fabc34d69fe4cef270d4871c66bf116ebd7f8b2d
mhuin <mhuin@redhat.com>
2018-04-11 15:13:37 +0200

    [WIP] SAML2 integration Todo: - release notes - idp metadata management
    TG-1176
    
    Change-Id: I8b833cc59ba512a25b669ae6cf41a99baa0ffc03
    Depends-On: Iaff2540d0b5585fb26dfa5eb48d6b69c05e1617f

---
+++ scripts/generate_sp_metadata.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Adapted from https://github.com/UNINETT/mod_auth_mellon/blob/master/mellon_create_metadata.sh
+
+set -e
+
+ENTITYID="$1"
+BASEURL="$2"
+CERT_FILE="$2"
+
+CERT="$(grep '^-----' "$CERTF_FILE")"
+
+cat >/etc/httpd/saml2/mellon_metadata.xml <<EOF
+<EntityDescriptor entityID="$ENTITYID" xmlns="urn:oasis:names:tc:SAML:2.0:metadata" xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+  <SPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
+    <KeyDescriptor use="signing">
+      <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+        <ds:X509Data>
+          <ds:X509Certificate>$CERT</ds:X509Certificate>
+        </ds:X509Data>
+      </ds:KeyInfo>
+    </KeyDescriptor>
+    <SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="$BASEURL/logout"/>
+    <AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="$BASEURL/postResponse" index="0"/>
+  </SPSSODescriptor>
+</EntityDescriptor>
+EOF
+
+umask 0777
+chmod go+r /etc/httpd/saml2/mellon_metadata.xml
