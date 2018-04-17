35209c7a896540abe0a5d0919261cfd9cbdcadfc
Fabien Boucher <fboucher@redhat.com>
2018-04-17 12:25:11 +0200

    Add in default ACL "addPatchSet" for Admin and Project Owner
    Gerrit 2.14 set this for "Registered Users" but our default ACL
    removed it at the initialization. Thus this patch adds again the ACL
    but only for Administrator and Project Owners groups.
    
    At repository level ACL this right can be set to Registered User if
    needed.
    
    This patch also remove the 'upgrade' part of gerrit-set-default-acl.sh
    that is un-used.
    
    Change-Id: If4e44b7719f18080a8f469c8dc6f273187d24d81

--- scripts/gerrit-set-default-acl.sh
+++ scripts/gerrit-set-default-acl.sh
@@ -14,14 +14,7 @@ pushd $CLONETEMPDIR
 git remote add origin ssh://gerrit/All-Projects
 git fetch origin refs/meta/config:refs/remotes/origin/meta/config
 git checkout meta/config
-cmsg="Provide the default config"
-[ "$1" = "upgrade" ] && {
-    [ -f rules.pl ] && git rm rules.pl
-    cmsg="Auto upgrade default config"
 
-    git config -f project.config --replace-all access.refs/meta/config.read "group Registered Users" "group Registered Users"
-    git config -f project.config --replace-all access.refs/meta/config.read "group Anonymous Users" "group Anonymous Users"
-}
 # As this we be called by ansible if gerrit.war change then
 # be sure to not overwrite project.config if modified
 # by someone else.
@@ -29,13 +22,13 @@ cmsg="Provide the default config"
     cp /usr/share/sf-config/defaults/project.config .
 }
 
-
 git status
 git diff
 git add *
 
 # git returns exit code 1 in case there is nothing to do
 # This fails during an upgrade, thus always return exit code 0
+cmsg="Provide the default config"
 git commit -a -m"$cmsg" || true
 git push origin meta/config:meta/config
 popd
