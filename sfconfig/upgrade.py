f14d7a81552e6251ccea7437e470c2c7fafbb8b1
Tristan Cacqueray <tdecacqu@redhat.com>
2018-04-30 08:36:11 +0000

    sfconfig: add support for external config repos
    Change-Id: I2db993ba2c5e7372322d448032b9eee0a5710b56

--- sfconfig/upgrade.py
+++ sfconfig/upgrade.py
@@ -128,6 +128,10 @@ def update_sfconfig(args):
         data["zuul"]["git_connections"] = []
         dirty = True
 
+    if "config-locations" not in data:
+        data["config-locations"] = {'config-repo': '', 'jobs-repo': ''}
+        dirty = True
+
     args.save_sfconfig = dirty
 
     if data['authentication']['admin_password'] == 'CHANGE_ME' or \
