d06a356b34b1734c4bee05fb8bd13a3e16c04e73
Nicolas Hicher <nhicher@redhat.com>
2018-05-04 16:48:29 -0400

    Fix to allow updating fqdn
    - add variable udpate_fqdn to sf-config
    - add statement to update fqdn in ansible roles
    
    When changing fqdn in /etc/software-factory/sfconfig.yaml, some
    modification must be done to ensure everything is correct:
    
    - change hostname in ssh pubkey (used later in group_vars/all.yaml) in
      update.yaml playbook
    - update emails in gerrit database
    - set hostname in sf-base
    - fix warn false statement for command
    - gitreview must be updated during the setup step or sync_* tasks will fail
    
    Change-Id: I3343cf4e9da15c7b1005bb205ec0528a41afebe0

--- sfconfig/cmd.py
+++ sfconfig/cmd.py
@@ -186,6 +186,12 @@ def main():
     sfconfig.arch.process(args)
     sfconfig.inventory.generate(args)
 
+    # Check if fqdn should be updated
+    if os.path.isfile(allyaml):
+        previous_args = yaml_load(allyaml)
+        if args.sfconfig['fqdn'] != previous_args['fqdn']:
+            args.sfconfig['update_fqdn'] = True
+
     # Generate group vars
     sfconfig.groupvars.load(args)
     for host in args.sfarch["inventory"]:
