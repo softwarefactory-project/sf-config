4084cffa8e7bc352858764f0c29a3c6fcc1d4df2
Tristan Cacqueray <tdecacqu@redhat.com>
2018-07-02 04:07:33 +0000

    zuul: automatically use git.zuul-ci.org when using upstream_zuul_jobs
    Change-Id: I49c406177e479cae383cad7f71335fa0db07774f

--- ansible/roles/sf-install-server/meta/sfconfig.py
+++ ansible/roles/sf-install-server/meta/sfconfig.py
@@ -103,6 +103,8 @@ class InstallServer(Component):
                 return
         if name == "local-cgit":
             baseurl = "http://%s/cgit" % host["hostname"]
+        elif name == "git.zuul-ci.org":
+            baseurl = "https://git.zuul-ci.org"
         else:
             baseurl = "file:///var/lib/software-factory/git"
         git_connections.append({
@@ -215,7 +217,7 @@ class InstallServer(Component):
         if args.sfconfig["zuul"]["upstream_zuul_jobs"]:
             # When using the upstream project, look for the connection name
             zuul_name = "openstack-infra/zuul-jobs"
-            zuul_loc = "https://git.openstack.org/openstack-infra/zuul-jobs"
+            zuul_loc = "https://git.zuul-ci.org/zuul-jobs"
             zuul_conn = None
             # Check if review.openstack.org is configured
             for connection in args.sfconfig.get("zuul", {}).get(
@@ -231,8 +233,10 @@ class InstallServer(Component):
                         zuul_conn = connection["name"]
                         break
             if not zuul_conn:
-                fail("To use upstream zuul-jobs, configure connection to the"
-                     " remote git")
+                # Automatically adds git.zuul-ci.org connection
+                zuul_name = "zuul-jobs"
+                zuul_conn = "git.zuul-ci.org"
+                self.ensure_git_connection(zuul_conn, args, host)
         else:
             # When using the zuul-jobs copy, set the right connection name
             zuul_name = "zuul-jobs"
