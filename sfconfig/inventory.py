38040a7634e87ffb126501706bfcf5fbfbb07ebd
Nicolas Hicher <nhicher@redhat.com>
2018-12-03 16:00:31 -0500

    Use rdo openstack-queens repository
    - Ensure openstack-release package is installed from rdo, use package from
      https://rdoproject.org for it supports both rhel and centos
    - This repos should be installed by both sfconfig and sf-base for we
      need this repo to install ara deps
    - Remove scl installation statement for rhel for it's done during
      registration process on sf-docs
    
    Change-Id: I3c2fd0989505c221992821b3467118922f09e486

--- sfconfig/inventory.py
+++ sfconfig/inventory.py
@@ -548,6 +548,28 @@ def enable_ara():
     os.environ["ARA_DIR"] = "/var/lib/software-factory/ansible/ara/"
 
 
+def install_openstack_repos():
+    '''Install openstack repository from rdoproject.org/repos
+    support both centos and rhel'''
+    openstack_release = 'queens'
+    rdo_repos_filename = '/etc/yum.repos.d/rdo-release.repo'
+    rdo_repos_base_url = 'https://rdoproject.org/repos'
+    rdo_repos_subdir = 'openstack-%s' % openstack_release
+    rdo_release_rpm = 'rdo-release-%s.rpm' % openstack_release
+    rdo_openstack_rpm = '%s/%s/%s' % (rdo_repos_base_url, rdo_repos_subdir,
+                                      rdo_release_rpm)
+    # TODO: we should use localinstall with yum or yum will failed with 'Error
+    # Nothing to do' when installing rpm from url. Could be converted to
+    # install when we will use dnf with centos/rhel 8 for dnf doesn't have the
+    # same issue.
+    if os.path.isfile(rdo_repos_filename) and \
+       openstack_release in open(rdo_repos_filename).read():
+        return
+    if "centos" or 'rhel' in open("/etc/os-release").read():
+        sfconfig.utils.execute(["yum", "localinstall", "-y",
+                                rdo_openstack_rpm])
+
+
 def install_ansible(args):
     if args.update:
         # Update ansible early on if possible
@@ -558,9 +580,6 @@ def install_ansible(args):
     if "centos" in open("/etc/os-release").read():
         sfconfig.utils.execute(
             ["yum", "install", "-y", "centos-release-scl-rh"])
-    else:
-        # TODO: auto-enable scl on rhel
-        pass
 
     try:
         # Replace system ansible with rh-python35
@@ -587,6 +606,7 @@ def run(args):
                "ansible-playbook", playbook_path]
     if not args.skip_apply:
         os.chdir("/")
+        install_openstack_repos()
         install_ansible(args)
         enable_ara()
         sfconfig.utils.execute(run_cmd)
