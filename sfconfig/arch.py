d4df81bfbfb573c3ab8519c9825e50b4e4f6d5d5
Nicolas Hicher <nhicher@redhat.com>
2018-12-12 14:54:01 -0500

    sfconfig: fails if dlrn is in arch.yaml for rhel deployment
    DLRN could not be installed on rhel for a depends issue (mock is not yet
    available). sfconfig will fail if dlrn is in arch on rhel systems.
    
    Change-Id: I1fe4dc80209a93296f1f89c01490365ddceffa44

--- sfconfig/arch.py
+++ sfconfig/arch.py
@@ -77,6 +77,11 @@ def process(args):
         if len(args.glue["roles"][requirement]) > 1:
             fail("Only one instance of %s is required" % requirement)
 
+    if 'rhel' in open("/etc/os-release").read() and "dlrn" in args.glue[
+            "roles"]:
+        fail("""please remove DLRN component from arch.yaml, it is not
+        supported on RHEL deployment""")
+
     if len(args.sfarch["inventory"]) > 1 and (
             "cgit" not in args.glue["roles"] and
             "gerrit" not in args.glue["roles"]):
