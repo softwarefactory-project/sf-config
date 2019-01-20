0a86e5f2855c365134c52a22f5e16a1a8a50004a
Tristan Cacqueray <tdecacqu@redhat.com>
2019-01-20 23:49:15 +0000

    zuul: fix tenant config-update secret generation
    This change fixes the script for:
    
    - new zuul keystorage uses keys/secrets/project/.. path
    - fix the missing priv key detection
    - store secrets in zuul home to ensure proper backup/restore
    
    Change-Id: I0668e54a0470ce14dc8cf642c0e1f0857148a09e

--- ansible/roles/sf-zuul/files/generate-tenant-update-secrets.py
+++ ansible/roles/sf-zuul/files/generate-tenant-update-secrets.py
@@ -52,12 +52,14 @@ def zuul_encrypt(private_key_file, secret_file):
     import subprocess
     pub_key = zuul.lib.encryption.deserialize_rsa_keypair(
         open(private_key_file, "rb").read())[1]
-    with open("%s.pub" % private_key_file, "wb") as pub:
+    pub_file = "/var/lib/zuul/tenant-secrets/%s.pub" % (
+        "%s.pub" % (private_key_file.replace('/', ':')))
+    with open(pub_file, "wb") as pub:
         pub.write(
             zuul.lib.encryption.serialize_rsa_public_key(pub_key))
     p = subprocess.Popen(
-        ["/usr/share/sf-config/scripts/zuul-encrypt-secret.py", "%s.pub" %
-         private_key_file, "ssh_private_key", "--infile", ssh_key],
+        ["/usr/share/sf-config/scripts/zuul-encrypt-secret.py",
+         pub_file, "ssh_private_key", "--infile", ssh_key],
         stdout=subprocess.PIPE)
     p.wait()
     return p.stdout.read().decode('utf-8')
@@ -68,13 +70,21 @@ for tenant, inf in tenants.items():
         # This is a local tenant, no need for tenant-update job
         continue
 
-    tenant_secret_path = "/var/lib/software-factory/bootstrap-data/" \
-                         "tenant-update_%s_secret.yaml" % tenant
+    tenant_secret_path = "/var/lib/zuul/tenant-secrets/" \
+        "config-update_%s_secret.yaml" % tenant
     os.makedirs(os.path.dirname(tenant_secret_path), exist_ok=True)
+
+    # Check for sf<3.2 legacy paths
+    old_path = "/var/lib/software-factory/bootstrap-data/" \
+        "tenant-update_%s_secret.yaml" % tenant
+    if os.path.exists(old_path):
+        os.rename(old_path, tenant_secret_path)
+
     if (
             os.path.exists(tenant_secret_path) and
             "pkcs" in open(tenant_secret_path).read()):
         continue
+
     print("Generating secret for tenant %s" % tenant, file=sys.stderr)
 
     req = urllib.request.urlopen(inf["url"] + "/resources")
@@ -86,7 +96,7 @@ for tenant, inf in tenants.items():
     tenant_config_name = tenant_config["config-repo"][len(
         connections[tenant_config_connection]["base-url"]):]
 
-    private_key_file = "/var/lib/zuul/keys/%s/%s.pem" % (
+    private_key_file = "/var/lib/zuul/keys/secrets/project/%s/%s/0.pem" % (
         tenant_config_connection, tenant_config_name)
 
     print("Looking for %s key on connection %s (%s)" % (
@@ -98,7 +108,7 @@ for tenant, inf in tenants.items():
         if os.path.exists(private_key_file):
             break
         time.sleep(1)
-    if retry == 59:
+    if retry == 1799:
         print("Couldn't find %s" % private_key_file)
         exit(1)
 
