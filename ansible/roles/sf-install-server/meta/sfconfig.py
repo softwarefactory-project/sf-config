0210ffd31a95809c1cdc1d3d7c184b39911935cf
Nicolas Hicher <nhicher@redhat.com>
2019-06-04 11:13:33 -0400

    sf-install-server: use gerrit connection name based on gateway_url
    When deploying tenant sf instance, conn_name should use the local gerrit
    connection instead master one.
    
    Co-authored-by: Fabien Boucher <fboucher@redhat.com>
    
    Change-Id: Ied5a7b2eac37c9bd3872e8f0d0a1977f01350e76

--- ansible/roles/sf-install-server/meta/sfconfig.py
+++ ansible/roles/sf-install-server/meta/sfconfig.py
@@ -221,6 +221,11 @@ class InstallServer(Component):
                 # If gerrit is enabled, use it's connection and default push
                 # location
                 conn_name = "gerrit"
+                for name, cnx in args.glue.get(["resources_connections"], {}).items():
+                    if name == "__force_dict__":
+                        continue
+                    if args.glue["gateway_url"] in cnx["base-url"]:
+                        conn_name = name
                 url = "%s/r/config" % args.glue["gateway_url"]
                 conf_loc = "git+ssh://gerrit/config"
                 jobs_loc = "git+ssh://gerrit/sf-jobs"
