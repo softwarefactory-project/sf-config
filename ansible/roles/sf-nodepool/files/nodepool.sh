deb0d2ae8eb97086b96dc5907fed25fdc360cb8b
Tristan Cacqueray <tdecacqu@redhat.com>
2019-09-23 13:53:50 +0000

    Switch to python3 packages
    This change replace SCL bits by the native python3 rpms.
    Also fixes the new ansible include_task interface.
    
    Depends-On: https://softwarefactory-project.io/r/16617
    Depends-On: https://softwarefactory-project.io/r/16592
    Depends-On: https://softwarefactory-project.io/r/16593
    Depends-On: https://softwarefactory-project.io/r/16595
    Depends-On: https://softwarefactory-project.io/r/16598
    Depends-On: https://softwarefactory-project.io/r/16600
    Depends-On: https://softwarefactory-project.io/r/16601
    Change-Id: I55e86aae1241f8c42b174eb6de4f00688533cc97

--- ansible/roles/sf-nodepool/files/nodepool.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-exec scl enable rh-python35 -- /opt/rh/rh-python35/root/bin/nodepool \
-     -s /etc/opt/rh/rh-python35/nodepool/secure.conf \
-     -c /etc/opt/rh/rh-python35/nodepool/nodepool.yaml $*
