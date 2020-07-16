20fd3fa0c8f6ed7f0c3332efa0492e255b11d438
danpawlik <dpawlik@redhat.com>
2020-07-16 14:43:52 +0200

    Added backup and restore feature for Kibana
    With this commit, Kibana saved_objects like dashboard or
    visualization will be backuped and restore after upgrade.
    
    Change-Id: Ica68388259df13d0371f27219e914538dac4807a

---
+++ ansible/roles/sf-kibana/files/backup-kibana.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+KIBANA_HOST=${KIBANA_HOST:-'localhost:5601'}
+BACKUP_DIR=${KIBANA_BACKUP_DIR:-'/usr/share/sf-config/kibana-backup'}
+
+# POSSIBLE doc_types: config map canvas-workpad canvas-element index-pattern visualization search dashboard url
+# NOTE: If there are no objects available in Kibana, don't break the script.
+for doc_type in visualization dashboard; do
+    curl -X POST "${KIBANA_HOST}/api/saved_objects/_export" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '{
+        "type": "'"$doc_type"'",
+        "excludeExportDetails": true,
+        "includeReferencesDeep": false
+    }' > "${BACKUP_DIR}/${doc_type}.ndjson" || true
+done
---
+++ ansible/roles/sf-kibana/files/restore-kibana.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KIBANA_HOST=${KIBANA_HOST:-'localhost:5601'}
+BACKUP_DIR=${KIBANA_BACKUP_DIR:-'/usr/share/sf-config/kibana-backup'}
+ALLOW_OVERWRITE=${KIBANA_ALLOW_OVERWRITE:-'true'}
+DATE=$(date '+%Y-%m-%d')
+
+# POSSIBLE doc_types: config map canvas-workpad canvas-element index-pattern visualization search dashboard url
+for doc_type in visualization dashboard; do
+    if [ -f "${BACKUP_DIR}/${doc_type}.ndjson" ]; then
+        curl -X POST "${KIBANA_HOST}/api/saved_objects/_import?overwrite=$ALLOW_OVERWRITE" -H 'kbn-xsrf: true'  --form file=@"${BACKUP_DIR}/${doc_type}.ndjson"
+        # Change the backup name to other, to avoid once restore
+        mv "${BACKUP_DIR}/${doc_type}.ndjson" "${BACKUP_DIR}/${doc_type}-${DATE}.ndjson"
+    fi
+done
+
