397cef8743366707b975325c1159f513df17973c
danpawlik <dpawlik@redhat.com>
2020-07-16 14:43:52 +0200

    Added backup and restore feature for Kibana
    With this commit, Kibana saved_objects like dashboard or
    visualization will be backuped and restore after upgrade.
    
    Change-Id: Ica68388259df13d0371f27219e914538dac4807a

---
+++ ansible/roles/sf-kibana/files/kibana-backup.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+# Original source: https://github.com/selivan/kibana-backup-simple
+# Small improvements are done in fork:
+# https://github.com/danpawlik/kibana-backup-simple
+
+# Kibana documentation:
+# https://www.elastic.co/guide/en/kibana/current/saved-objects-api-export.html
+# https://www.elastic.co/guide/en/kibana/current/saved-objects-api-import.html
+
+import json
+import sys
+import time
+import argparse
+import requests
+
+# Error message from Kibana listing all possible saved objects types:
+# \"type\" must be one of:
+# [config, map, canvas-workpad, canvas-element, index-pattern,
+#  visualization, search, dashboard, url]
+saved_objects_types = (
+    'config', 'map', 'canvas-workpad', 'canvas-element', 'index-pattern',
+    'visualization', 'search', 'dashboard', 'url')
+
+
+def backup(kibana_url, space_id, user, password, backup_dir):
+    """Return string with newline-delimitered json containing
+    Kibana saved objects"""
+    saved_objects = {}
+    if len(space_id):
+        url = kibana_url + '/s/' + space_id + '/api/saved_objects/_export'
+    else:
+        url = kibana_url + '/api/saved_objects/_export'
+    for obj_type in saved_objects_types:
+        r = requests.post(
+            url,
+            auth=(user, password),
+            headers={'Content-Type': 'application/json',
+                     'kbn-xsrf': 'reporting'},
+            data='{ "type": "' + obj_type + '" }'
+        )
+        r.raise_for_status()  # Raises stored HTTPError, if one occurred.
+        saved_objects[obj_type] = r.text
+        if backup_dir:
+            with open(("%s.ndjson" % obj_type), 'a') as f:
+                f.write(r.text)
+
+    with open("backup.ndjson", 'a') as f:
+        f.write('\n'.join(saved_objects.values()))
+
+    return '\n'.join(saved_objects.values())
+
+
+def restore(kibana_url, space_id, user, password, text, resolve_conflicts):
+    """Restore given newline-delimitered json containing
+    saved objects to Kibana"""
+
+    if len(space_id):
+        url = (kibana_url + '/s/' + space_id +
+               '/api/saved_objects/_import?overwrite=true')
+    else:
+        url = kibana_url + '/api/saved_objects/_import?overwrite=true'
+    print('POST ' + url)
+    for kib_obj in text:
+        print("Working on %s" % kib_obj)
+
+        if check_if_empty(kib_obj):
+            print("Spotted empty object. Continue...")
+            continue
+
+        r = make_request(url, user, password, kib_obj)
+
+        if not r:
+            print("Can not import %s into Kibana" % kib_obj)
+            continue
+
+        response_text = json.loads(r.text)
+        if not response_text['success'] and resolve_conflicts:
+            text = remove_reference(kib_obj)
+            r = make_request(url, user, password, text)
+
+        print(r.status_code, r.reason, '\n', r.text)
+        r.raise_for_status()  # Raises stored HTTPError, if one occurred.
+
+
+def remove_reference(text):
+    text = json.loads(text)
+    new_references = []
+    for ref in text['references']:
+        if not ref['id'].startswith('AX') and len(ref['id']) != 20:
+            new_references.append(ref)
+    text['references'] = new_references
+    return json.dumps(text)
+
+
+def make_request(url, user, password, text, retry=True):
+    r = None
+    try:
+        r = requests.post(
+            url,
+            auth=(user, password),
+            headers={'kbn-xsrf': 'reporting'},
+            files={'file': ('backup.ndjson', text)},
+            timeout=10
+        )
+    except requests.exceptions.ReadTimeout:
+        if not retry:
+            print("Importing failed. Retrying...")
+            time.sleep(10)
+            make_request(url, user, password, text)
+    return r
+
+
+def check_if_empty(text):
+    text = json.loads(text)
+    if 'exportedCount' in text and text['exportedCount'] == 0:
+        return True
+
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser(
+        description='Backup and restore Kibana saved objects. '
+                    'Writes backup to stdout and reads from stdin.')
+    args_parser.add_argument('action', choices=['backup', 'restore'])
+    args_parser.add_argument('--kibana-url', default='http://127.0.0.1:5601',
+                             help='URL to access Kibana API')
+    args_parser.add_argument('--space-id', default='',
+                             help='Kibana space id. If not set then the '
+                                  'default space is used.')
+    args_parser.add_argument('--user', default='', help='Kibana user')
+    args_parser.add_argument('--password', default='', help='Kibana password')
+    args_parser.add_argument('--backup-dir',
+                             help='Dir where backups will be stored')
+    args_parser.add_argument('--restore-file', help='ndjson file to restore')
+    args_parser.add_argument('--resolve-conflicts', default=True,
+                             help='Resolve conflicts by removing index '
+                                  'id reference in backup file')
+    args = args_parser.parse_args()
+
+    kibana_url = args.kibana_url
+    if (not args.kibana_url.startswith('http') and
+            not args.kibana_url.startswith('https')):
+        kibana_url = "http://%s" % args.kibana_url
+
+    if args.action == 'backup':
+        print(backup(kibana_url, args.space_id, args.user, args.password,
+                     args.backup_dir)
+              )
+    elif args.action == 'restore':
+        if args.restore_file:
+            with open(args.restore_file) as f:
+                text = f.readlines()
+        else:
+            text = ''.join(sys.stdin.readlines())
+
+        restore(kibana_url, args.space_id, args.user, args.password,
+                text, args.resolve_conflicts)
