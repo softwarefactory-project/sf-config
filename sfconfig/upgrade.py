a790d8bed19f8c3443152de409ab542b1cf3a28d
danpawlik <dpawlik@redhat.com>
2020-08-03 14:40:53 +0200

    Switching to Opendistro ELK stack in sf.io
    We would like to set RBAC for Kibana and one of the solution
    that gives us that is Opendistro.
    
    TG-3628
    
    Depends-On: https://softwarefactory-project.io/r/19155
    Depends-On: https://softwarefactory-project.io/r/19292
    
    Change-Id: I14aecbf48b8cc9d2d69dff33b092d83aa1eab226

--- sfconfig/upgrade.py
+++ sfconfig/upgrade.py
@@ -243,6 +243,30 @@ def update_sfconfig(args):
         open(args.config, 'w').write(re.sub(
             "admin_password:.*", "admin_password: %s" % new_pass, raw_config))
 
+    if 'elasticsearch_password' not in data['elasticsearch']:
+        data['elasticsearch']['elasticsearch_password'] = 'admin'
+        dirty = True
+
+    if 'kibanaserver_password' not in data['elasticsearch']:
+        data['elasticsearch']['kibanaserver_password'] = 'kibanasever'
+        dirty = True
+
+    if 'logstash_password' not in data['elasticsearch']:
+        data['elasticsearch']['logstash_password'] = 'logstash'
+        dirty = True
+
+    if 'readonly_user' not in data['elasticsearch']:
+        data['elasticsearch']['readonly_user'] = 'kibana'
+        dirty = True
+
+    if 'readonly_password' not in data['elasticsearch']:
+        data['elasticsearch']['readonly_password'] = 'kibana'
+        dirty = True
+
+    if 'repoxplorer_password' not in data['elasticsearch']:
+        data['elasticsearch']['repoxplorer_password'] = 'repoxplorer'
+        dirty = True
+
 
 def runc_provider_exists():
     runc = ''
