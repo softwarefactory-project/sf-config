ed5d6e13f9288d83b1b78454118d0e74d631bfb4
danpawlik <dpawlik@redhat.com>
2020-08-03 14:40:53 +0200

    Switching to Opendistro ELK stack in sf.io
    We would like to set RBAC for Kibana and one of the solution
    that gives us that is Opendistro.
    
    TG-3628
    
    Depends-On: https://softwarefactory-project.io/r/#/c/19359/
    Depends-On: https://softwarefactory-project.io/r/#/c/19561/
    Depends-On: https://softwarefactory-project.io/r/#/c/19589/
    
    Change-Id: I14aecbf48b8cc9d2d69dff33b092d83aa1eab226

--- sfconfig/upgrade.py
+++ sfconfig/upgrade.py
@@ -231,6 +231,20 @@ def update_sfconfig(args):
         }
         dirty = True
 
+    elasticsearch_credentials = ['elasticsearch_password',
+                                 'kibanaserver_password',
+                                 'logstash_password',
+                                 'readonly_password',
+                                 'repoxplorer_password',
+                                 'curator_password']
+
+    for credential in elasticsearch_credentials:
+        if ((credential in data['elasticsearch']
+             and data['elasticsearch'][credential] == 'CHANGE_ME')
+                or credential not in data['elasticsearch']):
+            data['elasticsearch'][credential] = uuid.uuid4().hex
+            dirty = True
+
     args.save_sfconfig = dirty
 
     if data['authentication']['admin_password'] == 'CHANGE_ME' or \
