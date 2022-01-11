c800e90aa0cb875d644462588db5aa5b17184364
Tristan Cacqueray <tdecacqu@redhat.com>
2022-01-11 14:33:07 +0000

    gerrit: upgrade to version 3.4.0
    Depends-On: https://softwarefactory-project.io/r/c/software-factory/pynotedb/+/23607
    Depends-On: https://softwarefactory-project.io/r/c/software-factory/managesf/+/23585
    Depends-On: https://softwarefactory-project.io/r/c/software-factory/sf-ci/+/23586
    Change-Id: Icef5c6c1aee63bcf44bde08e64330004c0783551

--- ansible/roles/sf-gerrit/files/entrypoint.sh
+++ ansible/roles/sf-gerrit/files/entrypoint.sh
@@ -1,24 +1,19 @@
 #!/bin/bash -e
 
-if [ ! -d /var/gerrit/git/All-Projects.git ] || [ "$1" == "init" ]; then
+# The /dev/./urandom is not a typo. https://stackoverflow.com/questions/58991966/what-java-security-egd-option-is-for
+JAVA_OPTIONS="-Djava.security.egd=file:/dev/./urandom"
+
+if [ -f /var/gerrit/logs/.run_init ]; then
     echo "Initializing Gerrit site ..."
-    java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war init \
-         -d /var/gerrit --batch --no-auto-start --skip-plugins
-    java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
+    java ${JAVA_OPTIONS} -jar /var/gerrit/bin/gerrit.war init -d /var/gerrit --batch --no-auto-start --skip-plugins
+    java ${JAVA_OPTIONS} -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
+    echo "Installing the plugins ..."
+    unzip -jo /var/gerrit/bin/gerrit.war WEB-INF/plugins/* -d /var/gerrit/plugins
+    for plugin in /var/gerrit-plugins/*; do
+        cp -uv $plugin /var/gerrit/plugins/
+    done
+    rm -f /var/gerrit/logs/.run_init
 fi
 
-if [ ! -f /var/gerrit/plugins/download-commands.jar ]; then
-    echo "Extract plugins for gerrit.war for they were present in pkg"
-    mkdir /tmp/gerrit
-    cd /tmp/gerrit
-    unzip /var/gerrit/bin/gerrit.war
-    mv WEB-INF/plugins/*jar /var/gerrit/plugins/
-    cd && rm -rf /tmp/gerrit
-fi
-
-if [ "$1" != "init" ]; then
-    echo "Reindex Gerrit ..."
-    java $JAVA_OPTS -jar /var/gerrit/bin/gerrit.war reindex -d /var/gerrit
-    echo "Running Gerrit ..."
-    exec /var/gerrit/bin/gerrit.sh run
-fi
+echo "Running Gerrit ..."
+exec java ${JAVA_OPTIONS} -jar /var/gerrit/bin/gerrit.war daemon -d /var/gerrit
