4da505c7aa867c75e49607d460386cc861496812
Daniel Pawlik <dpawlik@redhat.com>
2022-07-19 14:04:48 +0200

    Improve ci-log-processing role
    The role was not working correctly with external Elasticsearch
    service.
    
    Change-Id: I8175c8680b4cef17c437ef658cc602099d678a2b

---
+++ ansible/roles/sf-log-processing/meta/sfconfig.py
@@ -0,0 +1,34 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import urllib.parse
+
+from sfconfig.components import Component
+
+
+class LogProcessing(Component):
+    def configure(self, args, host):
+        external_elk = args.sfconfig.get('external_elasticsearch', {})
+
+        host = external_elk.get('host', None)
+        args.glue['external_elasticsearch_host'] = \
+            urllib.parse.urlparse(host).hostname if host else None
+        args.glue['external_elasticsearch_port'] = \
+            urllib.parse.urlparse(host).port if host else None
+
+        if external_elk.get('cacert_path', None):
+            args.glue['external_elasticsearch_cacert'] = \
+                external_elk.get('cacert_path')
+
+        if external_elk.get('users', None):
+            args.glue['external_elasticsearch_users'] = \
+                external_elk.get('users')
